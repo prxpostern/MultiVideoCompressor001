dcb5587572fda9d663fa8befc9d2f671a7745aa3
prxpostern <44132439+prxpostern@users.noreply.github.com>
2021-10-03 13:38:57 +0330

    Update start.py

--- start.py
+++ start.py
@@ -2,29 +2,19 @@ from telethon import TelegramClient, events, Button
 from download_from_url import download_file, get_size
 from file_handler import send_to_transfersh_async, progress
 import os
-import cryptg   #For Increasing speed of File Downloading
 import time
 import datetime
 import aiohttp
 import asyncio
-from tools import execute
 
 api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
 bot_token =os.environ.get("BOT_TOKEN")
                           
 download_path = "Downloads/"
-""" Global Variables """
-global ext0
-global ext1
-global ext2
-global ffcmd0
-global ffcmd1
-global ffcmd2
-global file_path
-
 
 bot = TelegramClient('Uploader bot', api_id, api_hash).start(bot_token=bot_token)
+
 
 @bot.on(events.NewMessage(pattern='/start'))
 async def start(event):
@@ -36,107 +26,48 @@ async def start(event):
             "Backup Channel":"https://t.me/disneygroubackup"}
     buttons = [[Button.url(k, v)] for k,v in dict_.items()]
 
-    await event.respond('Hi!')
+    await event.respond('Hi!\nMy Name Is Disney Team Transfer Uploader Bot Send me any file or direct download link and I upload and get the transfer.sh download link Bot Made by ❤ In 🇮🇳India by [Doreamonfans](https://t.me/doreamonfans2)', buttons=buttons)
     raise events.StopPropagation
 
-@bot.on(events.NewMessage)
+@bot.on(events.NewMessage(pattern='/encode'))
 async def echo(update):
     """Echo the user message."""
-    msg = await update.respond("Processing Plz Wait😁...")
+    msg = await update.respond("Send Your Media or URL Link to Start Encoding...")
+    async with bot.conversation(update.message.chat_id) as cv:
+        update2 = await cv.wait_event(events.NewMessage(update.message.chat_id))
 
     try:
         if not os.path.isdir(download_path):
             os.mkdir(download_path)
             
         start = time.time()
-        if not update.message.message.startswith("/") and not update.message.message.startswith("http") and update.message.media:
-            ext0 = "aaa"
-            ffcmd0 = "aaa"
-            file_path = await bot.download_media(update.message, download_path, progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+        
+        if not update2.message.message.startswith("/") and not update2.message.message.startswith("http") and update2.message.media:
+            await msg.edit("**Downloading starting😉...**")
+            file_path = await bot.download_media(update2.message, download_path, progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
                 progress(d, t, msg, start)))
-
-        elif not update.message.message.startswith("http") and not update.message.media and update.message.message.startswith(".") :
-            ext0 = "bbb"
-            ext2 = update.text
-            ffcmd0 = "aaa"
-        elif not update.message.message.startswith("http") and not update.message.media and update.message.message.startswith("-") :
-            ffcmd0 = "bbb"
-            ffcmd2 = update.text
-            ext0 = "bbb"
         else:
-            ext0 = "aaa"
-            ffcmd0 = "aaa"
-            msg = await update.respond("Processing Plz Wait😁...")
-            url = update.text
+            url = update2.text
             filename = os.path.join(download_path, os.path.basename(url))
-            file_path = await download_file(update.text, filename, msg, start, bot)
+            file_path = await download_file(update2.text, filename, msg, start, bot)
+            
+        print(f"file downloaded to {file_path}")
+        try:
+            await msg.edit(f"{file_path}")
 
+        except Exception as e:
+            print(e)
+            await msg.edit(f"Uploading Failed\n\n**Error:** {e}")
+        finally:
+            os.remove(file_path)
+            print("Deleted file :", file_path)
     except Exception as e:
         print(e)
-        await msg.edit(f"Download link is invalid or not accessable \n\n **Error:** {e}")
-    
-    """ User Input Section """
-    await asyncio.sleep(2)
-    if ext0 == "aaa" and ffcmd0 == "aaa":
-      await msg.reply("**Enter Extension with dot: like .mkv .mp4 .mp3 .aac .mka**")
-      async with bot.conversation(update.message.chat_id) as cv:
-        ext1 = await cv.wait_event(events.NewMessage(update.message.chat_id))
-        #ext2 = await ext1
-        
-    if ext0 == "bbb" and ffcmd0 == "aaa":
-      await msg.reply(
-          f"**Enter FFmpeg Options: like **\n\n`-sn -vn -c:a copy` \n\n `-sn -vn -c:a libmp3lame -ar 48000 -ab 256k` \n\n `-c:s copy -c:a copy -c:v libx264` \n\n `-c:v libx264 -s 320*240 -c:a libmp3lame -ar 48000 -ab 64k`"
-        )
-      async with bot.conversation(update.message.chat_id) as cvv:
-        ffcmd1 = await cvv.wait_event(events.NewMessage(update.message.chat_id))
-        #ffcmd2 = await ffcmd1
-        #await mtemp2.delete()
-    
-    """ Encoding Section """
-    if ext0 == "bbb" and ffcmd0 == "bbb":
-      await asyncio.sleep(2)
-      #ext3 = ext1.text
-      #ffcmd3 = ffcmd1.text
-      ponlyname = os.path.splitext(file_path)[0]
-      file_loc2 = f"{ponlyname}{ext2}"
-      size = os.path.getsize(file_loc2)
-      size_of_file = get_size(size)
-      name = os.path.basename(file_loc2)
-      ffcmd4 = f"ffmpeg -i {file_path} {ffcmd2} {file_loc2} -y"
-      await msg.edit(f"{ffcmd4}\n\nEncoding ...\n\n{size_of_file}\n\n**plz wait😍...**")
-      await asyncio.sleep(2)
-      
-      out, err, rcode, pid = await execute(f"{ffcmd4}")
-      if rcode != 0:
-        await msg.edit("**Error Occured. See Logs for more info.**")
-        print(err)
-              
-      """ Uploading Media Section """
-      await asyncio.sleep(2)
-      
-      #onlyfilename = os.path.splitext(name)[0]
-
-      await msg.edit(f"**Name: **`{name}`\n is Uploading ....**")
-            
-      c_time = time.time()    
-      try:
-        await bot.send_file(
-          update.message.chat_id,
-          file=file_loc2,
-          caption=f"`{name}` \n `{size_of_file}`",
-          reply_to=update.message
-        )
-      except Exception as e:
-        print(e)
-        await msg.edit(f"Uploading Failed\n\n**Error:** {e}")
-
-      """ Cleaning Section """
-      os.remove(file_path)
-      os.remove(file_loc2)
+        await msg.edit(f"Download link is invalid or not accessable contact my [owner](https://t.me/doreamonfans1)\n\n**Error:** {e}")
 
 def main():
     """Start the bot."""
-    print("\nBot started ...\n")
+    print("\nBot started visit @disneygrou For more updates...\n")
     bot.run_until_disconnected()
 
 if __name__ == '__main__':
